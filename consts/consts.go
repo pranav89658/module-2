e9b4e4677eeddeb25f6a76c2ad1d76c5ae002cb0
pranav89658 <163645156+pranav89658@users.noreply.github.com>
2024-03-18 14:45:59 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "pranavrao"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "PRANAV"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "PRO"
 )
 
 var ID ids.ID
